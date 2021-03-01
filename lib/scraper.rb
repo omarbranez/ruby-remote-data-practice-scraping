1ea5a313a92b24736140e9b0c94f454c1748f21c
Omar Branez <omar.branez@gmail.com>
2021-03-01 12:55:52 -0500

    commit tree 5e7428ef6a1dc7dcc422120d38cc78516f37ce36

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,13 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+doc.css(".headline-26OIBN").text
+
+courses = doc.css(".title-oE5vT4")
+courses.each do |course|
+    puts course.text.strip
+end
+
+p doc.css(".title-oE5vT4")[0]
